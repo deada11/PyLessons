de404408c04e37bde3ae3483f2966d8b43cb937b
deada11 <deada11@gmail.com>
2020-10-01 19:02:45 +0300

    Очередная задача

--- SecondCourseOfPython/quiz-2_1.5.py
+++ SecondCourseOfPython/quiz-2_1.5.py
@@ -1,4 +1,11 @@
-# 1) capacity - максимальное значение копилки. Присваиваете его в ините. А также присваиваете 0, как начальное значение копилки
-# 2) вызываете в функции add функцию can_add
-# 3) в функции can_add проверяете, что если вы добавите монеты к текущему значению копилки, то оно не будет больше capacity. Возвращаете True\False
-# 4) если can_add вернула True, то к текущему значению прибавляете v
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.count = 0
+
+    def can_add(self, v):
+        return self.count + v <= self.capacity
+
+    def add(self, v):
+        if self.can_add(v):
+            self.count += v
