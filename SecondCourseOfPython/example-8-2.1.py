f6f1c54b5614ab8f68790a4f3b488fe7b6d2311e
deada11 <deada11@gmail.com>
2020-10-12 18:41:14 +0300

    More examples

--- SecondCourseOfPython/example-8-2.1.py
+++ SecondCourseOfPython/example-8-2.1.py
@@ -1,8 +1,11 @@
+class BadName(Exception):
+    pass
+
 def greet(name):
     if name[0].isupper():
         return 'Hello, ' + name
     else:
-        raise ValueError(name + ' is inappropriate name')
+        raise BadName(name + ' is inappropriate name')
 
 while True:
     try:
