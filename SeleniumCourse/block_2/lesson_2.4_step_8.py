04a46cae127b6dae29cbae91fa17a6cc3cd7bd01
deada11 <deada11@gmail.com>
2020-12-11 01:40:23 +0300

    Поправил стиль кода, удалил лишние строки, добавил переходов на новые строки. Завершил блок 2 по курсу селениума.

--- SeleniumCourse/block_2/lesson_2.4_step_8.py
+++ SeleniumCourse/block_2/lesson_2.4_step_8.py
@@ -9,7 +9,7 @@ browser = webdriver.Chrome()
 browser.get(link)
 try:
     button = browser.find_element_by_id('book')
-    WebDriverWait(browser, 12).until(
+    WebDriverWait(browser, 13).until(
         EC.text_to_be_present_in_element((By.ID,'price'),'$100'))
     button.click()
 
