09e431ca69d3dff49b8d0ef97ffecf82b0b5d869
deada11 <deada11@gmail.com>
2020-12-16 21:22:17 +0300

    Добавил третий метод проверки наличия кнопки. Расписал разницу в комментариях.

--- SeleniumCourse/block_3/lesson_3_6_step_9_quiz/test_items.py
+++ SeleniumCourse/block_3/lesson_3_6_step_9_quiz/test_items.py
@@ -5,7 +5,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 # ---- Первый метод проверки наличия кнопки добавления в корзину ----
 def test_button_add_to_basker_method_1(browser): # browser передается в тест как параметр
     browser.get(link)
-    # time.sleep(30) для проверок с различными языками (нужно ревьюеру)
+    time.sleep(30) # для проверок с различными языками (нужно ревьюеру)
     assert browser.find_element_by_class_name('btn-add-to-basket'), 'There is no button for add to basket!'
     # достаточно такого ассерта, потому что функция assert получит некий элемент и вернет булево значение; т.е. если
     # элемент не будет найден, assert получит None, тест упадет и будет выведено сообщение "There is no button!"
@@ -13,7 +13,7 @@ def test_button_add_to_basker_method_1(browser): # browser передается 
 # ---- Второй метод проверки наличия кнопки добавления в корзину ----
 # def test_button_add_to_basker_method_1(browser): # browser передается в тест как параметр
 #     browser.get(link)
-#     # time.sleep(30) для проверок с различными языками (нужно ревьюеру)
+#     # time.sleep(30) # для проверок с различными языками (нужно ревьюеру)
 #     assert len(browser.find_element_by_class_name('btn-add-to-basket').text) > 0, \
 #         'There is no button for add to basket!'
 #     # тут проверяется не только наличие элемента на странице, но и длина его текста
@@ -23,7 +23,7 @@ def test_button_add_to_basker_method_1(browser): # browser передается 
 # ---- Третий метод проверки наличия кнопки добавления в корзину ----
 # def test_button_add_to_basket_method_2(browser):
 #     browser.get(link)
-#     # time.sleep(30) для проверок с различными языками (нужно ревьюеру)
+#     # time.sleep(30) # для проверок с различными языками (нужно ревьюеру)
 #     button = browser.find_element_by_class_name('btn-add-to-basket')
 #     assert button.is_displayed() == True, 'There is no button for add to basket!'
 #     # это третий способ ассерта, он чуть более читаемый, но загроможден лишней переменной,
