edc87ad65e67dc9ba2dd86588f2790a3c60d6132
deada11 <deada11@gmail.com>
2022-02-04 01:31:43 +0300

    Решил пару задач из курса Core

---
+++ AnyLessons/PythonCoreCourse/CoreQuiz1.py
@@ -0,0 +1,3 @@
+numbers = [14, 5, 6, 8, 17, 28]
+
+print(max(numbers) + min(numbers))
---
+++ AnyLessons/PythonCoreCourse/CoreQuiz2.py
@@ -0,0 +1,9 @@
+s = input()
+
+
+def hashtagGen(text):
+
+    return '#' + text.replace(" ", "")
+
+
+print(hashtagGen(s))
