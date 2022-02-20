1b09c1f6e7348607dd157f1f21ceaefd492438a7
deada11 <deada11@gmail.com>
2022-02-20 23:17:05 +0300

    Задачка на подсчет изменений в тексте.

---
+++ AnyLessons/PyDataQuiz2.py
@@ -0,0 +1,7 @@
+text = "Amy has 128 dollars, while David has 54 dollars. Amy is going to the zoo. David watches soccer."
+
+old = input()
+new = input()
+
+print(text.count(old))
+print(text.replace(old, new))
